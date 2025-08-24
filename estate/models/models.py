b7ca68c45d71722bdce2cf59e4c6d4a06b1cdaa2
PisethC <chuonpiseth05@gmail.com>
2025-08-24 16:17:03 +0700

    first commit

---
+++ estate/models/models.py
@@ -0,0 +1,30 @@
+from odoo import models, fields, api
+
+
+class estate(models.Model):
+    _name = 'estate.estate'
+    _description = 'estate.estate'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string='Date Availability')
+    expected_price = fields.Float(string='Expected Price')
+    selling_price = fields.Float(string='Selling Price')
+    bedrooms = fields.Integer(string='Bedrooms')
+    living_area = fields.Float(string='Living Area')
+    facades = fields.Integer(string='Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Float(string='Garden Area')
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ],
+        string='Garden Orientation',
+        default='north',
+    )
+
