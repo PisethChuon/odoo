3bce5e14f137d90fbcd6a891f36d96df87bdf9c0
PisethC <chuonpiseth05@gmail.com>
2025-08-25 22:49:17 +0700

    update

--- estate1/models/models.py
+++ estate1/models/models.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class estate1(models.Model):
-#     _name = 'estate1.estate1'
-#     _description = 'estate1.estate1'
+class estate1(models.Model):
+    _name = 'estate1.estate1'
+    _description = 'estate1.estate1'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    sale_person_id = fields.Many2one(
+        "res.partner",
+        string="Sale Person"
+    )
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
+
+class SalePerson(models.Model):
+    _name = 'estate1.property.type'
+    _description = 'Sele Person'
+
+    name = fields.Char(string='Name', required=True)
 
